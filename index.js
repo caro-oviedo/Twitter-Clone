5acab1a6a186599a682b30acec27ac81ab46c686
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2024-03-20 22:00:04 -0300

    Updated getFeedHtml function

--- index.js
+++ index.js
@@ -12,29 +12,41 @@ function getFeedHtml(){
     let feedHtml = ``
     tweetsData.forEach(function(tweet){
         feedHtml += `
-            <div class="tweet">
-                <div class="tweet-inner">
-                    <img src="${tweet.profilePic}" class="profile-pic">
-                    <div>
-                        <p class="handle">${tweet.handle}</p>
-                        <p class="tweet-text">${tweet.tweetText}</p>
-                        <div class="tweet-details">
-                            <span class="tweet-detail">
-                                ${tweet.replies.length}
-                            </span>
-                            <span class="tweet-detail">
-                                ${tweet.likes}
-                            </span>
-                            <span class="tweet-detail">
-                                ${tweet.retweets}
-                            </span>
-                        </div>   
-                    </div>            
-                </div>
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"
+                            data-like="${tweet.uuid}"
+                            ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"
+                            data-retweet="${tweet.uuid}"
+                            ></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
             </div>
-            `
-   })
-   return feedHtml 
-}
+        </div>
+        `
+           })
+           return feedHtml 
+        }
+        
 
-console.log(getFeedHtml())
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
