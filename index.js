0cff2690d96793ab1a5d2a7a92ba5ba66a793d7b
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2024-03-23 00:06:08 -0300

    Added HandleRetweetClick function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { tweetsData } from './data.js'
-
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
@@ -11,65 +10,84 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
     }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
 })
 
 function handleLikeClick(tweetId){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
     })[0]
-    
+
     if (targetTweetObj.isLiked){
-        targetTweetObj.likes--      
+        targetTweetObj.likes--
     }
     else{
-        targetTweetObj.likes++        
+        targetTweetObj.likes++ 
     }
-
     targetTweetObj.isLiked = !targetTweetObj.isLiked
-
     render()
 }
 
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+
+}
+
 function getFeedHtml(){
-      
     let feedHtml = ``
+    
     tweetsData.forEach(function(tweet){
         feedHtml += `
-        <div class="tweet">
-            <div class="tweet-inner">
-                <img src="${tweet.profilePic}" class="profile-pic">
-                <div>
-                    <p class="handle">${tweet.handle}</p>
-                    <p class="tweet-text">${tweet.tweetText}</p>
-                    <div class="tweet-details">
-                        <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots"
-                            data-reply="${tweet.uuid}"
-                            ></i>
-                            ${tweet.replies.length}
-                        </span>
-                        <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"
-                            data-like="${tweet.uuid}"
-                            ></i>
-                            ${tweet.likes}
-                        </span>
-                        <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet"
-                            data-retweet="${tweet.uuid}"
-                            ></i>
-                            ${tweet.retweets}
-                        </span>
-                    </div>   
-                </div>            
-            </div>
-        </div>
-        `
-           })
-           return feedHtml 
-        }
-        
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
 
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
 }
+
+render()
+
